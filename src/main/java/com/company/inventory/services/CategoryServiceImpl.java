e446d455d607b4f00d2fa3bf4ebe3de5554f0b3d
JoseCP82 <josecamarap@gmail.com>
2023-07-25 16:04:49 +0200

    servicio obtener todas las categorias

---
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -0,0 +1,35 @@
+package com.company.inventory.services;
+
+import com.company.inventory.dao.ICategoryDao;
+import com.company.inventory.model.Category;
+import com.company.inventory.response.CategoryResponseRest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements ICategoryService {
+
+    @Autowired
+    private ICategoryDao categoryDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public ResponseEntity<CategoryResponseRest> search() {
+        CategoryResponseRest response = new CategoryResponseRest();
+        try {
+            List<Category> categories = (List<Category>) categoryDao.findAll();
+            response.getCategoryResponse().setCategories(categories);
+            response.setMetadata("Respuesta ok", "00", "Respuesta exitosa");
+        } catch (Exception e) {
+            response.setMetadata("Respuesta No ok", "-1", "Error al consultar");
+            e.getStackTrace();
+            return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+    }
+}
