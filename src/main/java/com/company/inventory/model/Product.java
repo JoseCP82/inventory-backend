131f81c2079aa390c109123be5f076bf930ee432
JoseCP82 <josecamarap@gmail.com>
2023-07-28 12:40:31 +0200

    servicio save product

---
+++ src/main/java/com/company/inventory/model/Product.java
@@ -0,0 +1,33 @@
+package com.company.inventory.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name="product")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int account;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Category category;
+
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name="picture")
+    private byte[] picture;
+}
